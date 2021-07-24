f3396b73bd7f97ec7b66d847bbd2a32a5eb236a9
AhmedRoshdy <ahmedroshdy01029@gmail.com>
2021-07-24 18:15:13 +0200

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -1,3 +1,11 @@
+let city = document.querySelector(".weaher__city");
+let day = document.querySelector(".weather__day");
+let humidity = document.querySelector("weader__indicatoer--humiditiy>.value");
+let wind = document.querySelector(".weader__indicatoer--humiditiy>.value");
+let pressure = document.querySelector(".weader__indicatoer--pressure>.value");
+let image = document.querySelector(".weather__image");
+let temperaature = document.querySelector(".weather__temperature");
+
 let weatherAPIKey = "833a87b3be5a98f32e77368383c8a6b3";
 let weatherBaseEndpoint =
   "https://api.openweathermap.org/data/2.5/weather?units=metric&appid=" +
